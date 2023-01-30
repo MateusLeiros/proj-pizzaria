43be72b931e7bc89b7847d91534039317a3b89cc
Mateus Leiros <mateusleiros@gmail.com>
2023-01-30 15:56:14 -0300

    Novos templates iniciais

--- pizzaria/accounts/views.py
+++ pizzaria/accounts/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse('products')
+    return render(request, 'accounts/products.html')
 
 def customer(request):
-    return HttpResponse('customer')
+    return render(request, 'accounts/customer.html')

7e5d93fd0e86c85dd44c2b255a5e7608d6c6f8a2
Mateus Leiros <mateusleiros@gmail.com>
2023-02-14 14:14:47 -0300

    Modificações nas views para iniciar dinamicidade das paginas

--- pizzaria/accounts/views.py
+++ pizzaria/accounts/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    pedidos = Pedido.objects.all()
+    clientes = Cliente.objects.all()
+
+    total_clientes = clientes.count()
+    total_pedidos = pedidos.count()
+    entregues = pedidos.filter(status='Entregue').count()
+    aguardando = pedidos.filter(status='Aguardando').count()
+
+    context = {'pedidos':pedidos, 'clientes':clientes, 'total_pedidos':total_pedidos, 'entregues':entregues, 'aguardando':aguardando}
+
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    pizzas = Pizza.objects.all()
+    return render(request, 'accounts/products.html', {'pizzas':pizzas})
 
 def customer(request):
     return render(request, 'accounts/customer.html')

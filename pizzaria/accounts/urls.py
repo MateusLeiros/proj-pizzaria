5b43a539232e60473cb0af49b4169082f009aea6
Mateus Leiros <mateusleiros@gmail.com>
2023-02-27 16:18:30 -0300

    adicionada dinamicidade em alguns botoes da pagina principal

--- pizzaria/accounts/urls.py
+++ pizzaria/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('cliente/<str:pk>/', views.cliente),
+    path('', views.home, name="home"),
+    path('products/', views.products, name="products"),
+    path('cliente/<str:pk>/', views.cliente, name="cliente"),
 ]
